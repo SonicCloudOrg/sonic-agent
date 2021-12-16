e5b260269427129cfc92496901209dc350612411
ZhouYixun <291028775@qq.com>
2021-12-17 00:44:29 +0800

    save

--- src/main/java/com/sonic/agent/tests/android/AndroidTemperThread.java
+++ src/main/java/com/sonic/agent/tests/android/AndroidTemperThread.java
@@ -1,5 +1,16 @@
 package com.sonic.agent.tests.android;
 
-public class AndroidTemperThread {
+import com.android.ddmlib.IDevice;
+import com.sonic.agent.bridge.android.AndroidDeviceBridgeTool;
 
+import java.util.List;
+
+public class AndroidTemperThread extends Thread{
+    @Override
+    public void run() {
+        IDevice[] deviceList = AndroidDeviceBridgeTool.getRealOnLineDevices();
+        for(IDevice iDevice:deviceList){
+
+        }
+    }
 }
