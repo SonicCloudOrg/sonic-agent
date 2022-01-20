4cfcef78444195db850036d86b69c7aff5a41522
ZhouYixun <291028775@qq.com>
2022-01-20 21:51:41 +0800

    save

---
+++ src/main/java/org/cloud/sonic/agent/cv/H264Transfer.java
@@ -0,0 +1,9 @@
+package org.cloud.sonic.agent.cv;
+
+import org.bytedeco.javacv.FFmpegFrameGrabber;
+import org.bytedeco.javacv.FFmpegFrameRecorder;
+
+public class H264Transfer {
+    protected FFmpegFrameGrabber grabber = null;
+    protected FFmpegFrameRecorder recorder = null;
+}
