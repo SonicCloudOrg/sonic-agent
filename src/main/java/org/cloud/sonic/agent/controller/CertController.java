331ce29aa2ea7b76a7830c83003eb57dd2096dd8
ZhouYixun <291028775@qq.com>
2022-03-10 23:17:44 +0800

    proxy_download_page

--- src/main/java/org/cloud/sonic/agent/controller/CertController.java
+++ src/main/java/org/cloud/sonic/agent/controller/CertController.java
@@ -1,4 +1,19 @@
 package org.cloud.sonic.agent.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author ZhouYiXun
+ * @des 证书下载页
+ * @date 2022/3/10 23:04
+ */
+@Controller
 public class CertController {
+    @RequestMapping("/assets/download")
+    public String download(Model model) {
+        model.addAttribute("msg", "欢迎来到证书下载页面");
+        return "download";
+    }
 }
