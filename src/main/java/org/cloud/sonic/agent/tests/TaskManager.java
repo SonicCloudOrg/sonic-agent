ad72fbfa509a8455c170c637a45bf41e72fcb67a
JayWenStar <1599603313@qq.com>
2022-03-11 10:08:16 +0800

    🐛: 修复录屏内存泄露

--- src/main/java/org/cloud/sonic/agent/tests/TaskManager.java
+++ src/main/java/org/cloud/sonic/agent/tests/TaskManager.java
@@ -149,6 +149,13 @@ public class TaskManager {
      */
     public static void clearTerminatedThreadByKey(String key) {
         bootThreadsMap.remove(key);
+        Set<Thread> threads = childThreadsMap.get(key);
+        if (threads == null) {
+            return;
+        }
+        for (Thread thread : threads) {
+            thread.interrupt();
+        }
         childThreadsMap.remove(key);
     }
 
