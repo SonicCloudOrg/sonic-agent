acfcda225b2420c9abcf5b42691152d1ef857a46
ZhouYixun <291028775@qq.com>
2022-03-13 00:22:48 +0800

    update_manager

--- src/main/java/org/cloud/sonic/agent/tools/AgentManagerTool.java
+++ src/main/java/org/cloud/sonic/agent/tools/AgentManagerTool.java
@@ -1,9 +1,26 @@
 package org.cloud.sonic.agent.tools;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.stereotype.Component;
+
 /**
  * @author ZhouYiXun
  * @des Agent管理者
  * @date 2021/8/26 22:23
  */
+@Component
 public class AgentManagerTool {
+    private final Logger logger = LoggerFactory.getLogger(AgentManagerTool.class);
+
+    @Autowired
+    private ConfigurableApplicationContext context;
+
+    public void stop() {
+        context.close();
+        logger.info("Bye！");
+    }
 }
