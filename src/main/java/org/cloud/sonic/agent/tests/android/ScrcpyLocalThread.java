1421817bc4ab4659f1056db9b2a38b6ec87c7aa8
ZhouYixun <291028775@qq.com>
2022-03-13 17:46:56 +0800

    update_1.23

--- src/main/java/org/cloud/sonic/agent/tests/android/ScrcpyLocalThread.java
+++ src/main/java/org/cloud/sonic/agent/tests/android/ScrcpyLocalThread.java
@@ -71,14 +71,14 @@ public class ScrcpyLocalThread extends Thread {
 
     @Override
     public void run() {
-        File scrcpyServerFile = new File("plugins/scrcpy-server.jar");
+        File scrcpyServerFile = new File("plugins/sonic-android-scrcpy.jar");
         try {
-            iDevice.pushFile(scrcpyServerFile.getAbsolutePath(), "/data/local/tmp/scrcpy-server.jar");
+            iDevice.pushFile(scrcpyServerFile.getAbsolutePath(), "/data/local/tmp/sonic-android-scrcpy.jar");
         } catch (Exception e) {
             e.printStackTrace();
         }
         try {
-            iDevice.executeShellCommand("CLASSPATH=/data/local/tmp/scrcpy-server.jar app_process / com.genymobile.scrcpy.Server 1.21 log_level=info max_size=0 bit_rate=1800000 max_fps=60 lock_video_orientation=-1 tunnel_forward=true send_frame_meta=false control=true show_touches=false stay_awake=true power_off_on_close=false clipboard_autosync=false",
+            iDevice.executeShellCommand("CLASSPATH=/data/local/tmp/sonic-android-scrcpy.jar app_process / com.genymobile.scrcpy.Server 1.23 log_level=info max_size=0 bit_rate=2097152 max_fps=60 lock_video_orientation=-1 tunnel_forward=true send_frame_meta=false control=true show_touches=false stay_awake=true power_off_on_close=false clipboard_autosync=false",
                     new IShellOutputReceiver() {
                         @Override
                         public void addOutput(byte[] bytes, int i, int i1) {
