47d5242f798ff5ac48d029a54624f0ad0fafd939
ZhouYixun <291028775@qq.com>
2022-04-17 00:00:32 +0800

    微调

--- src/main/java/org/cloud/sonic/agent/tools/poco/PocoTool.java
+++ src/main/java/org/cloud/sonic/agent/tools/poco/PocoTool.java
@@ -1,3 +1,19 @@
+/*
+ *  Copyright (C) [SonicCloudOrg] Sonic Project
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
 package org.cloud.sonic.agent.tools.poco;
 
 import com.alibaba.fastjson.JSON;
@@ -106,10 +122,10 @@ public class PocoTool {
                         }
                         wait++;
                         if (wait >= 20) {
-                            webSocketClient.close();
                             break;
                         }
                     }
+                    webSocketClient.close();
                 }
             } catch (URISyntaxException e) {
                 e.printStackTrace();
