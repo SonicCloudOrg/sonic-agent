b60358225654f58d51040e5da27e9716cc705425
ZhouYixun <291028775@qq.com>
2022-04-19 01:10:44 +0800

    对接重构apk

--- src/main/java/org/cloud/sonic/agent/websockets/AndroidWSServer.java
+++ src/main/java/org/cloud/sonic/agent/websockets/AndroidWSServer.java
@@ -144,7 +144,7 @@ public class AndroidWSServer implements IAndroidWSServer {
         Thread touchPro = new Thread(() -> {
             try {
                 //开始启动
-                iDevice.executeShellCommand(String.format("CLASSPATH=%s exec app_process /system/bin org.cloud.sonic.android.SonicTouchService", finalPath)
+                iDevice.executeShellCommand(String.format("CLASSPATH=%s exec app_process /system/bin org.cloud.sonic.android.plugin.SonicPluginTouchService", finalPath)
                         , new IShellOutputReceiver() {
                             @Override
                             public void addOutput(byte[] bytes, int i, int i1) {
@@ -153,7 +153,7 @@ public class AndroidWSServer implements IAndroidWSServer {
                                 if (res.contains("Address already in use")) {
                                     NotStopSession.add(session);
                                 }
-                                if (res.contains("Server start")) {
+                                if (res.contains("starting：start()")) {
                                     isTouchFinish.release();
                                 }
                             }
@@ -172,6 +172,7 @@ public class AndroidWSServer implements IAndroidWSServer {
                         , iDevice.getSerialNumber());
                 logger.error(e.getMessage());
             }
+            System.out.println("end");
         });
         touchPro.start();
 
