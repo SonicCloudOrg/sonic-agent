b8bae6443d65af22245efd3581287aaac4f05628
ZhouYixun <291028775@qq.com>
2022-10-03 23:17:32 +0800

    feat: init groovy script1

--- src/main/java/org/cloud/sonic/agent/tests/script/GroovyTest.java
+++ src/main/java/org/cloud/sonic/agent/tests/script/GroovyTest.java
@@ -3,16 +3,18 @@ package org.cloud.sonic.agent.tests.script;
 import groovy.lang.Binding;
 import groovy.lang.GroovyShell;
 import org.cloud.sonic.driver.android.AndroidDriver;
+import org.cloud.sonic.vision.cv.SIFTFinder;
 
 public class GroovyTest {
     public static void main(String[] args) {
         Binding binding = new Binding();
-        AndroidDriver a;
-//        binding.setVariable("var", 5);
+        binding.setVariable("SIFTFinder", new SIFTFinder());
         GroovyShell gs = new GroovyShell(binding);
-        Object value = gs.evaluate("a = new AndroidDriver(\"http://localhost:8094\");" +
-                "println a.getSessionId();" +
-                "return 1"
+        Object value = gs.evaluate(
+                "import org.cloud.sonic.vision.cv.SIFTFinder;" +
+                        "SIFTFinder a = new SIFTFinder();" +
+                        "println a.toString();" +
+                        "return 1"
         );
         System.out.println(value.equals(1));
 //        System.out.println(binding.getVariable("abc").equals(123));
