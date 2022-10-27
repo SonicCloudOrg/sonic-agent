7897d8c1f345d8a406d614cf488e87c7966d996f
wunanfang <zhuifeng@douyu.tv>
2022-10-27 17:46:56 +0800

    fix:手动判空runningTestMapKey

--- src/main/java/org/cloud/sonic/agent/tests/SuiteListener.java
+++ src/main/java/org/cloud/sonic/agent/tests/SuiteListener.java
@@ -16,7 +16,6 @@
  */
 package org.cloud.sonic.agent.tests;
 
-import cn.hutool.core.util.StrUtil;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
@@ -39,7 +38,7 @@ public class SuiteListener implements ISuiteListener {
     @Override
     public void onStart(ISuite suite) {
         String runningTestsMapKey = getRunningTestsMapKey(suite);
-        if (StrUtil.isBlank(runningTestsMapKey)){
+        if (runningTestsMapKey == null || runningTestsMapKey.length() == 0){
             return;
         }
         runningTestsMap.put(runningTestsMapKey, true);
@@ -48,7 +47,7 @@ public class SuiteListener implements ISuiteListener {
     @Override
     public void onFinish(ISuite suite) {
         String runningTestsMapKey = getRunningTestsMapKey(suite);
-        if (StrUtil.isBlank(runningTestsMapKey)){
+        if (runningTestsMapKey == null || runningTestsMapKey.length() == 0){
             return;
         }
         // 加上udId，避免先完成的设备移除后，后完成的设备无法执行后续操作
