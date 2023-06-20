f072829c9fe89f0a76e876e0790272be280eaf30
caofengbin <1050430934@qq.com>
2023-06-20 19:33:10 +0800

    实现基于lanmda的插值器函数

--- src/test/java/org/cloud/sonic/agent/tests/handlers/AndroidTouchHandlerTest.java
+++ src/test/java/org/cloud/sonic/agent/tests/handlers/AndroidTouchHandlerTest.java
@@ -2,6 +2,8 @@ package org.cloud.sonic.agent.tests.handlers;
 
 import org.junit.Test;
 
+import java.util.function.BiFunction;
+
 public class AndroidTouchHandlerTest {
 
     private int width = 720;
@@ -26,5 +28,40 @@ public class AndroidTouchHandlerTest {
         int[] re = transferWithRotation(1095, 168);
         System.out.println(re[0]);
         System.out.println(re[1]);
+    }
+
+    @Test
+    public void test_interpolator() {
+        int[] re1 = {200, 1200};
+        int[] re2 = {200, 300};
+        // 过渡总时间
+        int duration = 500;
+        // 开始时间
+        long startTime = System.currentTimeMillis();
+        while (true) {
+            // 当前时间
+            long currentTime = System.currentTimeMillis();
+            float timeProgress = (currentTime - startTime) / (float) duration;
+            if (timeProgress >= 1.0f) {
+                // 已经过渡到结束值，停止过渡
+                System.out.println("[" + re2[0] + "," + re2[1] + "]");
+                break;
+            }
+            BiFunction<Integer, Integer, Integer> transitionX = (start, end) ->
+                    (int) (start + (end - start) * timeProgress);
+            BiFunction<Integer, Integer, Integer> transitionY = (start, end) ->
+                    (int) (start + (end - start) * timeProgress); // Y 坐标过渡函数
+
+            int currentX = transitionX.apply(re1[0], re2[0]); // 当前 X 坐标
+            int currentY = transitionY.apply(re1[1], re2[1]); // 当前 Y 坐标
+            // 使用当前坐标进行操作
+            // ...
+            System.out.println("[" + currentX + "," + currentY + "]");
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+
+            }
+        }
     }
 }
